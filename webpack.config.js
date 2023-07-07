6da5721e7808fdb5048fbe37d950d7222ae00f7a
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2023-07-07 04:14:55 -0600

    Define rules for css, images, fonts, txt files

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.png$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|ttf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.txt$/i,
+        type: "asset/source",
+      },
+    ],
+  },
 };
